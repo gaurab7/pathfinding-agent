42f3ca878084bfc8448b1d0dc80f0156e8a77651
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2025-07-09 20:02:51 +0545

    updates to scripts

--- enviroment.py
+++ enviroment.py
@@ -1,7 +1,12 @@
 import random
+from perlin_noise import PerlinNoise
+
 
 
 def generate_map(tile_width, tile_height, screen, tile):
     for y in range(0, screen.get_height(), tile_height):
          for x in range(0, screen.get_width(), tile_width):
              screen.blit(tile, (x, y))
+
+#path gen
+noise = PerlinNoise(octaves=4)
--- main.py
+++ main.py
@@ -9,6 +9,8 @@ pygame.init()
 width =1024
 height = 768
 screen = pygame.display.set_mode((width, height))#resolution-->4:3(1024x768)
+fps = 60
+clock = pygame.time.Clock()
 pygame.display.set_caption("Self-Driving Car Simulation")
 
 #loading assets
@@ -16,6 +18,8 @@ car = pygame.image.load("Mark III_Woods.png")
 grass = pygame.image.load("grass.png")
 tile_width = grass.get_width()
 tile_height = grass.get_height()
+obs = pygame.image.load("blck.png")  # Load your obstacle image
+
 # main loop-keeps the window open and the simulation running
 running = True
 while running:
@@ -26,9 +30,9 @@ while running:
 
    #fill the screen with grass
     generate_map(tile_width, tile_height, screen, grass)
-
     # update the display
     pygame.display.flip()
+    clock.tick(fps) #60 frames per second
 
 # quit pygame
 pygame.quit()
