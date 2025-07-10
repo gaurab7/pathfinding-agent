0c08073ebee1c005482d43995cacc38a3cbb7b8b
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2025-07-10 17:55:24 +0545

    modified map generation using perlin noise

--- enviroment.py
+++ enviroment.py
@@ -1,12 +1,27 @@
 import random
+import pygame
 from perlin_noise import PerlinNoise
 
-
-
-def generate_map(tile_width, tile_height, screen, tile):
-    for y in range(0, screen.get_height(), tile_height):
-         for x in range(0, screen.get_width(), tile_width):
-             screen.blit(tile, (x, y))
-
-#path gen
 noise = PerlinNoise(octaves=4)
+threshold = -0.095  # threshold for path vs obstacle
+scale = 60
+border = 5 # border size for tiles
+
+def generate_map(tile_size, screen):
+    for x in range(0, screen.get_width(), tile_size):
+        for y in range(0, screen.get_height(), tile_size):
+            # generate a map using Perlin noise
+            val = noise([x/scale, y/scale])#gives value between -1 and 1
+            if val > threshold:
+
+                # white tlie for path at (x,y) of size = tile_size
+                pygame.draw.rect(screen, (200,212,255), (x, y, tile_size-3, tile_size-3))
+            else:
+                # black tile for obstacle at (x,y) of size = tile_sizergb(204,231,232)
+                pygame.draw.rect(screen, (230,226,255), (x, y, tile_size-border, tile_size-border))
+
+            
+
+    
+
+
--- main.py
+++ main.py
@@ -8,17 +8,13 @@ pygame.init()
 #display setup
 width =1024
 height = 768
+tile_size = 32 #size of each tile in pixels
 screen = pygame.display.set_mode((width, height))#resolution-->4:3(1024x768)
 fps = 60
 clock = pygame.time.Clock()
 pygame.display.set_caption("Self-Driving Car Simulation")
 
-#loading assets
-car = pygame.image.load("Mark III_Woods.png")
-grass = pygame.image.load("grass.png")
-tile_width = grass.get_width()
-tile_height = grass.get_height()
-obs = pygame.image.load("blck.png")  # Load your obstacle image
+
 
 # main loop-keeps the window open and the simulation running
 running = True
@@ -28,8 +24,9 @@ while running:
             running = False
 
 
-   #fill the screen with grass
-    generate_map(tile_width, tile_height, screen, grass)
+   #fill the screen with black color
+    screen.fill((0, 0, 0))
+    generate_map(tile_size, screen)
     # update the display
     pygame.display.flip()
     clock.tick(fps) #60 frames per second
